82b5819c3e997576ce49fbf66ed34f800ba4c820
Sino <ilyas.alfan.b@gmail.com>
2017-01-18 15:28:01 +0100

    DbService

--- src/db_service.py
+++ src/db_service.py
@@ -1,1 +1,26 @@
-# EMPTY
+import psycopg2
+
+# The url for each connection
+url = "host='localhost' dbname='boat_wars' user='postgres' password='root'"
+
+# Establishes a new connection to execute the given query. Returns nothing.
+def execute(query):
+    establishedConnection = psycopg2.connect(url)
+    connCursor = establishedConnection.cursor()
+
+    connCursor.execute(query)
+    establishedConnection.close()
+
+# Establishes a new connection to execute the given query. Returns the
+# results from the given query. This should not be used for queries that
+# return no result such as INSERT or UPDATE queries. Prefer execute() for
+# INSERT and UPDATE queries.
+def query(query):
+    establishedConnection = psycopg2.connect(url)
+    connCursor = establishedConnection.cursor()
+
+    connCursor.execute(query)
+    results = connCursor.fetchall()
+
+    establishedConnection.close()
+    return results
