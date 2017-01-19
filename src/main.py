020ce5acd9cfa1a1d25cb92ea608596d9f897797
Sino <ilyas.alfan.b@gmail.com>
2017-01-19 11:25:37 +0100

    Main menu options now change screens depending on the selected option i.e 'start game' switches to the game screen whilst 'options' switches to the settings screen

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ import pygame
 from app import AppContext
 from game import Game
 
-from main_menu import MainScreen
-
 # Initializes pygame
 pygame.init()
 pygame.font.init()
@@ -19,14 +17,8 @@ pygame.display.set_caption(app.title)
 # We create a surface that will be set to the specified resolution
 surface = pygame.display.set_mode((app.width, app.height))
 
-# The screen to start the application with.
-# NOTE: if you're working on a separate screen (such as hiscores, you can simply
-# change the MainScreen() to your own implementation. Ensure however that your
-# implementation contains the `update()` and `draw()` methods
-screen = MainScreen(app, surface)
-
 # Creates the game and ignites the game loop
-game = Game(surface, screen)
+game = Game(app, surface)
 game.game_loop()
 
 # The game loop has ended, let's de-initialize the pygame modules
--- src/main_menu.py
+++ src/main_menu.py
@@ -1,9 +1,14 @@
 import pygame
 
+from highscores import HighscoresScreen
+from settings import SettingsScreen
+from getting_started import GettingStartedScreen
+
 class MainScreen:
-    def __init__(self, app, surface):
+    def __init__(self, game, app, surface):
+        self.game = game
         self.surface = surface
-        self.image = pygame.image.load('resources/main_menu.jpg')
+        self.image = pygame.image.load('resources/screens/main_menu.jpg')
         self.app = app
 
     # Draws the components of this main menu screen.
@@ -20,13 +25,14 @@ class MainScreen:
             y = mouse_pos[1]
 
             if x >= 350 and y >= 140 and x <= 670 and y <= 214:
-                print("START GAME") # TODO
+                from game import GameScreen
+                self.game.set_screen(GameScreen(self.surface))
             elif x >= 354 and y >= 250 and x <= 668 and y <= 320:
-                print("INSTRUCTIONS") # TODO
+                self.game.set_screen(GettingStartedScreen(self.surface))
             elif x >= 359 and y >= 355 and x <= 670 and y <= 428:
-                print("OPTIONS") # TODO
+                self.game.set_screen(SettingsScreen(self.surface))
             elif x >= 356 and y >= 458 and x <= 667 and y <= 538:
-                print("HIGHSCORES") # TODO
+                self.game.set_screen(HighscoresScreen(self.surface))
 
     # Updates this main menu screen.
     def update(self):
