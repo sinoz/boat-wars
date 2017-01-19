785f6c19cd71b2060950156b0ea11ebf089b0e38
Sino <ilyas.alfan.b@gmail.com>
2017-01-19 17:46:46 +0100

    Added mock insert values

--- sql/boat_wars.sql
+++ sql/boat_wars.sql
@@ -4,3 +4,11 @@ CREATE TABLE players(
 	name character varying NOT NULL,
 	score integer DEFAULT 0
 );
+
+INSERT INTO players (name, score) VALUES ('Mathijs', 3);
+INSERT INTO players (name, score) VALUES ('Ryan', 4);
+INSERT INTO players (name, score) VALUES ('Johan', 0);
+INSERT INTO players (name, score) VALUES ('Sam', 15);
+INSERT INTO players (name, score) VALUES ('Xin', 8);
+INSERT INTO players (name, score) VALUES ('Maurice', 2);
+
