2581851e7cc866c776300d295bd00c13a0c879b5
Sino <ilyas.alfan.b@gmail.com>
2017-01-24 12:23:05 +0100

    Updated highscores + boat_wars database SQL

--- sql/boat_wars.sql
+++ sql/boat_wars.sql
@@ -5,10 +5,16 @@ CREATE TABLE players(
 	score integer DEFAULT 0
 );
 
-INSERT INTO players (name, score) VALUES ('Mathijs', 3);
-INSERT INTO players (name, score) VALUES ('Ryan', 4);
-INSERT INTO players (name, score) VALUES ('Johan', 0);
-INSERT INTO players (name, score) VALUES ('Sam', 15);
-INSERT INTO players (name, score) VALUES ('Xin', 8);
-INSERT INTO players (name, score) VALUES ('Maurice', 2);
+ALTER TABLE players
+RENAME COLUMN score to wins;
+
+ALTER TABLE players
+ADD losses integer;
+
+INSERT INTO players (name, wins, losses) VALUES ('Mathijs', 21, 20);
+INSERT INTO players (name, wins, losses) VALUES ('Ryan', 10, 11);
+INSERT INTO players (name, wins, losses) VALUES ('Johan', 7, 7);
+INSERT INTO players (name, wins, losses) VALUES ('Sam', 15, 18);
+INSERT INTO players (name, wins, losses) VALUES ('Xin', 8, 6);
+INSERT INTO players (name, wins, losses) VALUES ('Maurice', 24, 30);
 
--- src/screens/highscores.py
+++ src/screens/highscores.py
@@ -5,16 +5,16 @@ import screens.sound as sound
 
 import widget.button
 
-HighscoresFetchQuery = 'SELECT name, score FROM players ORDER BY score DESC LIMIT 10;'
-
-class HighscoresScreen:
+HighscoresFetchQuery = "SELECT name, wins, losses, trim(from to_char(CAST(wins AS float) / CAST(losses AS float), \'99.00\')) AS ratio FROM players GROUP BY name, wins, losses ORDER BY ratio DESC LIMIT 10;"
+class HighscoresScreen: # TODO this needs optimization
     def __init__(self, game, prev=None):
         self.image = pygame.image.load('resources/screens/' + game.language + '/highscores.jpg')
         self.game = game
         self.prev = prev
-        self.font = pygame.font.SysFont("monospace", 42)
+        self.font = pygame.font.SysFont("monospace", 33, bold=True)
         self.return_button = widget.button.Button((16, 600), (93, 77), self.return_to_main)
         self.scores = db.db_service.query(HighscoresFetchQuery)
+        self.color = (0, 0, 0)
         sound.Plopperdeplop.music(self, 'high_scores')
 
     # Draws the components of this highscores screen.
@@ -22,10 +22,31 @@ class HighscoresScreen:
         self.game.surface.blit(self.image, (0, 0))
 
         offset = 0
+        count = 0
+        column = 0
+
         for result in self.scores:
-            label = self.font.render(str(result[0]) + ":" + str(result[1]), 1, (0, 0, 0))
-            self.game.surface.blit(label, (480, 190 + offset))
-            offset += 78
+            if count == 5:
+                count = 0
+                offset = 0
+                column += 1
+
+            x = ((column * 530) + 147)
+            y = (offset + 126)
+
+            name = result[0]
+            wins = result[1]
+            losses = result[2]
+            ratio = result[3]
+
+            nameWinsLossesDisplay = self.font.render(str(name) + " " + str(wins) + "/" + str(losses), 1, self.color)
+            ratioDisplay =  self.font.render("Ratio:" + str(ratio), 1, self.color)
+
+            self.game.surface.blit(nameWinsLossesDisplay, (x, y))
+            self.game.surface.blit(ratioDisplay, (x, y + 25))
+
+            count += 1
+            offset += 94
 
     # TODO
     def return_to_main(self, x, y, cursor):
