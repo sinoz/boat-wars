8311cdd5e1d317588f19149be524fe5156c3eaf9
Ryan Wilson <ryanwilson219@gmail.com>
2017-01-26 12:14:17 +0100

    Coding

--- src/play/session.py
+++ src/play/session.py
@@ -19,23 +19,23 @@ class Session:
         self.draw_type = 1
 
         # Adds three ships for player one
-        self.p1.add_ship(play.ship.Ship(grid.get(2, 2)))
-        self.p1.add_ship(play.ship.Ship(grid.get(10, 1), type=play.ship.QueenMary))
-        self.p1.add_ship(play.ship.Ship(grid.get(4, 6), type=play.ship.Avenger))
-        self.p1.add_ship(play.ship.Ship(grid.get(19, 1), type=play.ship.Avenger))
+        self.p1.add_ship(play.ship.Ship(grid.get(5, 0)))
+        self.p1.add_ship(play.ship.Ship(grid.get(10, 0), type=play.ship.QueenMary))
+        self.p1.add_ship(play.ship.Ship(grid.get(16, 0), type=play.ship.Avenger))
+        self.p1.add_ship(play.ship.Ship(grid.get(21, 0)))
 
         # And now we add three ships for player two
-        self.p2.add_ship(play.ship.Ship(grid.get(4, 16), type=play.ship.QueenMary))
-        self.p2.add_ship(play.ship.Ship(grid.get(12, 14), type=play.ship.Avenger))
-        self.p2.add_ship(play.ship.Ship(grid.get(7, 9), type=play.ship.Avenger))
-        self.p2.add_ship(play.ship.Ship(grid.get(17, 15)))
+        self.p2.add_ship(play.ship.Ship(grid.get(5, 19)))
+        self.p2.add_ship(play.ship.Ship(grid.get(10, 18), type=play.ship.Avenger))
+        self.p2.add_ship(play.ship.Ship(grid.get(16, 17), type=play.ship.QueenMary))
+        self.p2.add_ship(play.ship.Ship(grid.get(21, 19)))
 
         # Rotate the ships of player one to face the boats of player two
         self.p1.forEachShip(lambda ship: ship.transform(180))
 
         # set some ships to defense mode
         # grid.get(2, 2).ship.switch_defense_mode()
-        grid.get(12, 14).ship.switch_defense_mode()
+        # grid.get(12, 14).ship.switch_defense_mode()
 
     # Handles an event.
     def on_event(self, event):
