c57183f035f72df7cbe67efda7185c617a6ccffc
Ryan Wilson <ryanwilson219@gmail.com>
2017-01-27 13:38:24 +0100

    Database Building

--- sql/boat_wars.sql
+++ sql/boat_wars.sql
@@ -17,3 +17,66 @@ INSERT INTO players (name, wins, losses) VALUES ('Johan', 7, 7);
 INSERT INTO players (name, wins, losses) VALUES ('Sam', 15, 18);
 INSERT INTO players (name, wins, losses) VALUES ('Xin', 8, 6);
 INSERT INTO players (name, wins, losses) VALUES ('Maurice', 24, 30);
+
+
+
+DROP TABLE IF EXISTS Boats;
+CREATE TABLE Boats(
+    XPos integer,
+    YPos integer,
+    HP integer,
+    BType varchar(10),
+    State varchar(10),
+    BRange integer,
+    Attack integer,
+    ShotDef boolean,
+    MineDef boolean
+);
+
+DROP TABLE IF EXISTS Cards;
+CREATE TABLE Cards(
+    Card1var varchar(10),
+    Card1Type varchar(10),
+    Card1Lang varchar(10),
+    Card2var varchar(10),
+    Card2Type varchar(10),
+    Card2Lang varchar(10),
+    Card3var varchar(10),
+    Card3Type varchar(10),
+    Card3Lang varchar(10),
+    Card4var varchar(10),
+    Card4Type varchar(10),
+    Card4Lang varchar(10),
+    Card5var varchar(10),
+    Card5Type varchar(10),
+    Card5Lang varchar(10),
+    Card6var varchar(10),
+    Card6Type varchar(10),
+    Card6Lang varchar(10),
+);
+
+DROP TABLE IF EXISTS Turn;
+CREATE TABLE Turn(
+    Turn integer
+);
+
+INSERT INTO Boats(XPos, YPos, HP, BType, State, BRange, Attack, ShotDef, MineDef)
+VALUES (src.play.ship.Ship.x,
+    src.play.ship.Ship.y, src.play.ship.Ship.health, src.play.ship.Ship.mode, src.play.ship.Ship.firerange, src.play.ship.Ship.firepower, src.play.ship.Ship.shotdef, src.play.ship.Ship.minedef),
+(src.play.ship.Ship.x,
+    src.play.ship.Ship.y, src.play.ship.Ship.health, src.play.ship.Ship.mode, src.play.ship.Ship.firerange, src.play.ship.Ship.firepower, src.play.ship.Ship.shotdef, src.play.ship.Ship.minedef),
+(src.play.ship.Ship.x,
+    src.play.ship.Ship.y, src.play.ship.Ship.health, src.play.ship.Ship.mode, src.play.ship.Ship.firerange, src.play.ship.Ship.firepower, src.play.ship.Ship.shotdef, src.play.ship.Ship.minedef),
+(src.play.ship.Ship.x,
+    src.play.ship.Ship.y, src.play.ship.Ship.health, src.play.ship.Ship.mode, src.play.ship.Ship.firerange, src.play.ship.Ship.firepower, src.play.ship.Ship.shotdef, src.play.ship.Ship.minedef),
+(src.play.ship.Ship.x,
+    src.play.ship.Ship.y, src.play.ship.Ship.health, src.play.ship.Ship.mode, src.play.ship.Ship.firerange, src.play.ship.Ship.firepower, src.play.ship.Ship.shotdef, src.play.ship.Ship.minedef),
+(src.play.ship.Ship.x,
+    src.play.ship.Ship.y, src.play.ship.Ship.health, src.play.ship.Ship.mode, src.play.ship.Ship.firerange, src.play.ship.Ship.firepower, src.play.ship.Ship.shotdef, src.play.ship.Ship.minedef),
+(src.play.ship.Ship.x,
+    src.play.ship.Ship.y, src.play.ship.Ship.health, src.play.ship.Ship.mode, src.play.ship.Ship.firerange, src.play.ship.Ship.firepower, src.play.ship.Ship.shotdef, src.play.ship.Ship.minedef),
+(src.play.ship.Ship.x,
+    src.play.ship.Ship.y, src.play.ship.Ship.health, src.play.ship.Ship.mode, src.play.ship.Ship.firerange, src.play.ship.Ship.firepower, src.play.ship.Ship.shotdef, src.play.ship.Ship.minedef),
+
+INSERT INTO Cards(Card1, Card2, Card3, Card4, Card5, Card6)
+VALUES (src.play.card)
