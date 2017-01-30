c255f9f8e3a9f229baeaa3a9af4d098c322cb381
Ryan Wilson <ryanwilson219@gmail.com>
2017-01-30 15:01:13 +0100

    Database Building: Cards

--- src/play/crd.py
+++ src/play/crd.py
@@ -1,5 +1,5 @@
 import pygame
-
+import db.db_service
 import screens.game.cards
 
 class Card:
@@ -9,22 +9,14 @@ class Card:
         self.language = lang
         self.image = pygame.image.load('resources/cards/' + type + '/' + self.language + '/' + id + '.jpg')
 
-        # x = 0 + self.tile.x
-        # y = 0 + self.tile.y
-        # health = 0 + self.health
-        # typ3 = self.type
-        # mode = self.mode            #0 = Defense, 1 = Attack, for further coding.
-        # firerange = 0 + self.firerange
-        # firepower =0 + self.firepower
-        #                             # For the coming: False = Negative, True = Applied
-        # applied_smokescreen = self.applied_smokescreen
-        # mine_armor = self.mine_armor
-        # sabotage = self.sabotage
-        # remaining_tiles = self.remaining_tiles
+        # Name = self.id
+        # Type = self.type
+        # Lang = self.language
         #
-        # db.db_service.execute("INSERT INTO into Boats (XPos, YPos, HP, BType, State, BRange, Attack, ShotDef, MineDef, ReflDef, BoatMovementLeft) VALUES (" + str(x) + str(y) + str(health) + str(typ3) + str(mode) + str(firerange) + str(firepower) + str(applied_smokescreen) + str(mine_armor) + str(sabotage) + str(remaining_tiles));
-        #
+        # db.db_service.execute("INSERT INTO Cards (CName, CType, CLang) VALUES (" + str(Name) + "," + str(Type) + "," + str(Lang) + ");")
+
         # lang = English / Dutch
+
     # Draws this card on the given surface
     def draw(self, surface, pos):
         surface.blit(self.image, screens.game.cards.CardPositions[pos])
