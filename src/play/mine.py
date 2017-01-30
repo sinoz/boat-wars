95c9925031800652d44af7f530b729efa413dce9
JVDH1 <johan.hoeven@outlook.com>
2017-01-30 15:47:42 +0100

    Merge branch 'master' of https://github.com/sinoz/boat-wars
    # Conflicts:
    #	src/play/session.py

--- src/play/mine.py
+++ src/play/mine.py
@@ -5,7 +5,7 @@ class Mine:
         self.tile = tile
         self.x = tile.x
         self.y = tile.y
-        self.image = pygame.image.load('resources/templates/sea_mine.jpg')
+        self.image = pygame.image.load('resources/templates/Sea_mine.png')
         self.rect = pygame.rect.Rect(self.x * self.tile.width, self.y * self.tile.height, self.tile.width, self.tile.height)
         self.owner = None
         self.tile.set_mine(self)
